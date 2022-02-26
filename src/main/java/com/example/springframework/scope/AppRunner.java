1259054948936bd6f0627494269e0f7a432f0494
dev-hjkim <dev--test@naver.com>
2022-02-27 03:16:22 +0900

    220227, environment & profile

--- src/main/java/com/example/springframework/scope/AppRunner.java
+++ src/main/java/com/example/springframework/scope/AppRunner.java
@@ -4,13 +4,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Component
 public class AppRunner implements ApplicationRunner {
 
     @Autowired
     ApplicationContext ctx;
+
+    @Autowired
+    Proto proto;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
@@ -28,5 +34,9 @@ public class AppRunner implements ApplicationRunner {
         System.out.println(ctx.getBean(Single.class).getProto());
         System.out.println(ctx.getBean(Single.class).getProto());
         System.out.println(ctx.getBean(Single.class).getProto());
+
+        Environment environment = ctx.getEnvironment();
+        System.out.println(Arrays.toString(environment.getActiveProfiles()));
+        System.out.println(Arrays.toString(environment.getDefaultProfiles()));
     }
 }
--- src/main/java/com/example/springframework/scope/Proto.java
+++ src/main/java/com/example/springframework/scope/Proto.java
@@ -1,9 +1,12 @@
 package com.example.springframework.scope;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
-@Component @Scope(value = "prototype", proxyMode = ScopedProxyMode.TARGET_CLASS)
+@Component
+@Profile("test")
+@Scope(value = "prototype", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class Proto {
 }
