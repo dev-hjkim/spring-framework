3c2c6842cfd398178ef3a3dcc2779e0901c18550
dev-hjkim <dev--test@naver.com>
2022-02-27 03:39:19 +0900

    220227, messageSource(i18n)

--- src/main/java/com/example/springframework/SpringFrameworkApplication.java
+++ src/main/java/com/example/springframework/SpringFrameworkApplication.java
@@ -2,7 +2,10 @@ package com.example.springframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 @SpringBootApplication
 @PropertySource("classpath:/app.properties")
@@ -12,4 +15,12 @@ public class SpringFrameworkApplication {
         SpringApplication.run(SpringFrameworkApplication.class, args);
     }
 
+    @Bean
+    public MessageSource messageSource() {
+        var messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setCacheSeconds(3);
+        return messageSource;
+    }
 }
---
+++ src/main/java/com/example/springframework/i18n/I18nRunner.java
@@ -0,0 +1,26 @@
+package com.example.springframework.i18n;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Component;
+
+import java.util.Locale;
+
+@Component
+public class I18nRunner implements ApplicationRunner {
+
+    @Autowired
+    MessageSource messageSource;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        while (true) {
+            System.out.println(messageSource.getMessage("greeting", new String[]{"hjkim"}, Locale.KOREA));
+            System.out.println(messageSource.getMessage("greeting", new String[]{"hjkim"}, Locale.getDefault()));
+            Thread.sleep(1000);
+        }
+
+    }
+}
