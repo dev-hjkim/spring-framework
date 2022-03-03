6e7b83b8ba0e0456dff6a21c954564adb6de80e5
dev-hjkim <dev--test@naver.com>
2022-03-04 00:13:02 +0900

    220304, databind - remove code causing error

--- src/main/java/com/example/springframework/databind/WebConfig.java
+++ src/main/java/com/example/springframework/databind/WebConfig.java
@@ -1,9 +1,6 @@
 package com.example.springframework.databind;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ConversionServiceFactoryBean;
-import org.springframework.core.convert.ConversionService;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
